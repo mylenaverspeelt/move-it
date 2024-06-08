8ecb2fc25b5864bf88d3c694f405d6409d16d457
mylenaverspeelt <mylena49@hotmail.com>
2024-06-08 18:37:01 -0300

    create variables css / add default-layout css

--- move-it/src/app/components/default-layout/default-layout.component.ts
+++ move-it/src/app/components/default-layout/default-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-default-layout',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default-layout.component.scss']
 })
 export class DefaultLayoutComponent implements OnInit {
+
+@Input() title: string = ""
+@Input() primaryBtnText: string = ""
+@Input() secondaryBtnText: string = ""
 
   constructor() { }
 
