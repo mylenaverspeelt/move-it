c46f4f041daafb9bef68cc7d4b89632ec814ec8b
mylenaverspeelt <mylena49@hotmail.com>
2024-07-11 08:31:47 -0300

    fix: apiURL link

--- move-it/src/app/services/login.service.ts
+++ move-it/src/app/services/login.service.ts
@@ -8,7 +8,7 @@ import { tap } from 'rxjs';
 })
 export class LoginService {
 
-  apiUrl: string = "ec2-18-188-17-199.us-east-2.compute.amazonaws.com/auth"
+  apiUrl: string = "http://ec2-18-188-17-199.us-east-2.compute.amazonaws.com/auth"
 
   constructor(private httpClient: HttpClient) { }
 
